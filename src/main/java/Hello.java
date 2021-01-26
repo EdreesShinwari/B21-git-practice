18115db040012d3ecae471d8e126cbda3c52d5da
EdreesShinwari <ekshinwari@gmail.com>
2021-01-26 15:41:00 -0500

    master updated Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
         System.out.println("Hello World, B21");
         System.out.println("Patience is a virtue");
         //watching more movie in bedroom
+        //final we agree on conflict
 
     }
 }
