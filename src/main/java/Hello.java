9dcceb3f9a2a8b7a57f74378e93ac4ccff6c884c
EdreesShinwari <ekshinwari@gmail.com>
2021-01-26 15:28:10 -0500

    ConflictingBranch updated Hello.java by watching movie

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World, B21");
         System.out.println("Patience is a virtue");
+        //watching more movie in bedroom
+
     }
 }
